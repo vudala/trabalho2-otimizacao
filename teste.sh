a540a823d3a3a31d7f148cb0ba552c74d90288ab
edvs19 <edvs19@inf.ufpr.br>
2022-09-10 21:20:48 -0300

    script  funcionando

--- teste.sh
+++ teste.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-OPTIONS=
-TIMES=100
-SUM=0
-for i in {1..$TIMES};
+OPTIONS=a
+TIMES=1000.0
+SUM=0.0
+for i in $(seq 1 $TIMES);
 do
     TEMPO=$(./elenco -v -$OPTIONS < input.txt | grep Tempo | cut -d" " -f4)
-    echo $TEMPO
+    SUM=$(bc <<< "$SUM + $TEMPO")
 done
+
+echo Tempo médio de execução
+echo $(bc -l <<< "$SUM / $TIMES") ms
